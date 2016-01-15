55f02455ad91aa71aac2f86abbdd46a3ae4fd22d
neAs <neAs@alderaan>
2016-01-15 15:25:06 +1100

    initial commit

---
+++ dbPart.sql
@@ -0,0 +1,111 @@
+-- http://www.formget.com/ajax-image-upload-php/
+-- https://www.youtube.com/watch?v=tufJwoQ0Xuo
+-- http://stackoverflow.com/questions/3748/storing-images-in-db-yea-or-nay
+-- http://www.w3schools.com/php/php_file_upload.asp
+
+DROP DATABASE dbPart;
+CREATE DATABASE IF NOT EXISTS dbPart;
+USE dbPart;
+-- tbPart
+CREATE TABLE tbType (
+	id 			INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	type 		VARCHAR(4),
+	descr 		VARCHAR(255)
+) DEFAULT CHARSET=utf8 ENGINE=InnoDB;
+-- tbSupplier
+CREATE TABLE tbSupplier (
+	id 			INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	company 	VARCHAR(255), 
+	contact 	VARCHAR(255), 
+	address1 	VARCHAR(255),
+	address2 	VARCHAR(255),
+	city 		VARCHAR(255),
+	state 		VARCHAR(255),
+	country		VARCHAR(255),
+	postcode 	VARCHAR(255),
+	phone 		VARCHAR(30),
+	mobile		VARCHAR(30),
+	fax 		VARCHAR(30),
+	email 		VARCHAR(255),
+	www 		VARCHAR(255),
+	active		BOOLEAN DEFAULT TRUE
+) DEFAULT CHARSET=utf8 ENGINE=InnoDB;
+-- tbPart
+CREATE TABLE tbPart (
+	id				INT NOT NULL PRIMARY KEY AUTO_INCREMENT UNIQUE,
+	part_number		VARCHAR(255) NOT NULL UNIQUE,
+	descr			VARCHAR(255),
+	typeid			INT,
+	active			BOOLEAN DEFAULT TRUE,
+	FOREIGN KEY (typeid) REFERENCES tbType(id)
+) DEFAULT CHARSET=utf8 ENGINE=InnoDB AUTO_INCREMENT=300000;
+-- tbSupplierPart
+CREATE TABLE tbSupplierPart (
+--	id				INT NOT NULL AUTO_INCREMENT UNIQUE,
+	supplierid		INT NOT NULL,
+	partid			INT NOT NULL,
+	sup_part_number VARCHAR(255),
+	FOREIGN KEY (supplierid) REFERENCES tbSupplier(id),
+	FOREIGN KEY (partid) REFERENCES tbPart(id),
+	PRIMARY KEY (supplierid, partid)
+) DEFAULT CHARSET=utf8 ENGINE=InnoDB AUTO_INCREMENT=300000;
+-- tbDrawing
+CREATE TABLE tbDrawing (
+	id				INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	partid			INT,
+	drawing_number	VARCHAR(255) NOT NULL,
+	rev				VARCHAR(3) DEFAULT '0',
+	pdf				VARCHAR(255),
+	dwg				VARCHAR(255),
+	FOREIGN KEY (partid) REFERENCES tbPart(id)
+) DEFAULT CHARSET=utf8 ENGINE=InnoDB;
+
+-- #Fill Table with DATA
+INSERT into tbType (type, descr) VALUES ('RAW', 'Raw material');
+INSERT into tbType (type, descr) VALUES ('HYD', 'Hydraulic components');
+INSERT into tbType (type, descr) VALUES ('ELE', 'Electrical components');
+INSERT INTO tbType (type, descr) VALUES ('MEC', 'Mechanical components');
+INSERT INTO tbType (type, descr) VALUES ('PUR', 'Purchased components');
+INSERT into tbType (type, descr) VALUES ('POW', 'Truck, engine and PTO components');
+INSERT into tbType (type, descr) VALUES ('SUB', 'Sub contract requirement');
+
+INSERT INTO tbSupplier SET company = 'ASHDOWNS';
+INSERT INTO tbSupplier SET company = 'COLUSSI ENGINEERING';
+INSERT INTO tbSupplier SET company = 'JTD ENGINEERING';
+INSERT INTO tbSupplier SET company = 'POWERCLIPPER';
+INSERT INTO tbSupplier SET company = 'PRINCE';
+INSERT INTO tbSupplier SET company = 'TRANSCRETE';
+
+INSERT IGNORE INTO tbPart 
+SET part_number = CONCAT('P', 300000),
+	descr = 'RELAY CHANGE OVER  24V 30A 5 PIN', 
+	typeid = '3';
+
+INSERT IGNORE INTO tbPart 
+SET part_number = CONCAT('P', LAST_INSERT_ID()+1),
+	descr = 'INTERPUMP WS202', 
+	typeid = '5';
+
+INSERT IGNORE INTO tbPart 
+SET part_number = '35T-301',
+	descr = 'TROJAN CLEVIS PIN (3/4")', 
+	typeid = '4';
+
+INSERT IGNORE INTO tbPart 
+SET part_number = 'T0055',
+	descr = 'TRUCK SIDE DECK SHEET', 
+	typeid = '1';	
+
+INSERT IGNORE INTO tbSupplierPart (supplierid, partid, sup_part_number)
+VALUES
+(1, 300000,'0 332 209 203'),
+(3, 300002, 'SEAOARD 19.05 PIN'),
+(2, 300001, 'WS202'),
+(4, 300002, 'HARDENED PIN 3/4'),
+(6, 300002, '35T-301'),
+(5, 300003, 'DECK SIDE SHEET');
+	
+INSERT INTO tbDrawing (partid, drawing_number, rev) 
+VALUES 
+(300002, '35T-301', 'A'),
+(300003, 'T0055', 'B');
